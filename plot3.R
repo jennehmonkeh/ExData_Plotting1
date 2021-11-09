eccd3eeeb7dae49cb8ee3d10997cb8bbb7cd0509
jennehmonkeh <jennehmonk@gmail.com>
2021-11-09 18:47:34 -0500

    made the code prettier

--- plot3.R
+++ plot3.R
@@ -6,10 +6,11 @@ days <- subset(alldata, Date == "2/2/2007" | Date == "1/2/2007")
 
 days$datetime <- paste(days$Date,days$Time)
 days$datetime <- strptime(days$datetime, "%d/%m/%Y %H:%M:%S")
-with (days,plot(datetime,Sub_metering_1,type="n",xlab="",ylab="Energy sub metering"))
-lines(days$datetime,days$Sub_metering_1)
-lines(days$datetime,days$Sub_metering_2,col="red")
-lines(days$datetime,days$Sub_metering_3,col="blue")
+with (days,{plot(datetime,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
+lines(datetime,Sub_metering_2,col="red")
+lines(datetime,Sub_metering_3,col="blue")
+})
+
 legend("topright",lwd=1,col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.copy(png,file="plot3.png",width=480,height=480)
